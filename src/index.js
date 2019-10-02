09f2fab6e2a059b9a658a45cffbc3a8954042d89
Robin Doirin <robindoirin@gmail.com>
2019-10-02 14:21:37 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
+function init() {
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', addTodo);
+}
+
+function addTodo(e) {
+  e.preventDefault();
+  const text = document.getElementById('new-task-description');
+  addTolist(text.value);
+  text.value = "";
+}
+
+function addTolist(text) {
+  const list = document.getElementById('task');
+  const item = document.createElement('li');
+  intem.innerHTML = text;
+  list.appendChild(item)
+}
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  console.log('Page has loaded');
+  init();
 });
